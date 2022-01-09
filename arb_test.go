73c99f4842bb0cbec8f3bbf71b7b7ce2024857fe
Craig O'Donnell <cbodonnell@comcast.net>
2022-01-09 10:57:32 -0500

    more tests

--- arb_test.go
+++ arb_test.go
@@ -1,6 +1,7 @@
 package arb
 
 import (
+	"os"
 	"strings"
 	"testing"
 )
@@ -12,7 +13,7 @@ func TestRead(t *testing.T) {
 	reader := strings.NewReader(jsonString)
 	arb, err := Read(reader)
 	if err != nil {
-		t.Fatal("failed to read json string from reader", err)
+		t.Fatal("failed to read json string from reader:", err)
 	}
 	if arb["key"] != expected {
 		t.Fatalf("unexpected value for key %s, expected %s", arb["key"], expected)
@@ -25,9 +26,43 @@ func TestReadBytes(t *testing.T) {
 	expected := "value"
 	arb, err := ReadBytes([]byte(jsonString))
 	if err != nil {
-		t.Fatal("failed to read json string from bytes", err)
+		t.Fatal("failed to read json string from bytes:", err)
 	}
 	if arb["key"] != expected {
 		t.Fatalf("unexpected value for key %s, expected %s", arb["key"], expected)
+	}
+}
+
+// TestWrite tests writing from an io.Write
+func TestWrite(t *testing.T) {
+	arb := New()
+	arb["hello"] = "world"
+	arb["number"] = 2222
+	arb["nothing"] = nil
+	child := New()
+	child["isChild"] = true
+	arb["child"] = child
+
+	err := arb.Write(os.Stdout)
+	if err != nil {
+		t.Fatal("failed to write:", err)
+	}
+}
+
+// TestReadBytes tests reading from a byte slice
+func TestGetString(t *testing.T) {
+	arb := New()
+	arb["hello"] = "world"
+	arb["number"] = 2222
+	result, err := arb.GetString("hello")
+	expected := "world"
+	if err != nil {
+		t.Fatal("failed get string:", err)
+	}
+	if result != expected {
+		t.Fatalf("result was %s, expected %s", result, expected)
+	}
+	if _, err := arb.GetString("number"); err == nil {
+		t.Fatalf("number %d is not a string", arb["number"])
 	}
 }
